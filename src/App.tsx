0d86df6a6a3f5aa028038e4c7b05ac5a7298ace4
WillianDomingues79 <willian.domingues3@gmail.com>
2022-10-24 20:08:01 -0300

    Primeiro Commit

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+
+import styles from './App.module.css'
+
+
+function App() {
+  return (
+   <div>
+    <Header />
+    <main className={styles.main}>
+      <h1>Conteudo ...</h1>
+    </main>
+    
+    <Footer />
+   </div>
+  );
+}
+
+export default App;
